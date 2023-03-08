4cc0507976c7da9e5a0a0505d32b6ae2d253cd51
Jappananny <Jappananny@gmail.com>
2023-03-08 16:07:32 +0300

    SQL (команды)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,35 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Команды веденные в SQL Shell
+        //\l
+        // CREATE DATABASE skypro;
+        //\l
+        //\c skypro
+        //CREATE TABLE employee (
+        //id BIGSERIAL NOT NULL PRIMARY KEY,
+        //first_name VARCHAR(50) NOT NULL,
+        //last_name VARCHAR(50) NOT NULL,
+        //gender VARCHAR(6) NOT NULL.
+        //age INT NOT NULL
+        //);
+        //SELECT * FROM employee;
+        //INSERT INTO employee (
+        //first_name, last_name, gender, age)
+        //VALUES ('Ivan', 'Ivanov', 'male', 30);
+        //SELECT & FROM employee;
+        //INSERT INTO employee (
+        //first_name, last_name, gender, age)
+        //VALUES ('Elena', 'Morozova', 'female', 23);
+        //INSERT INTO employee (
+        //first_name, last_name, gender, age)
+        //VALUES ('Vasja', 'Arutinjan', 'male', 38);
+        //SELECT & FROM employee;
+        //UPDATE employee SET first_name = 'Oleg', last_name = 'Bodrov', gender = 'male', age = 50 WHERE id = 2);
+        //SELECT & FROM employee;
+        //DELETE FROM employee WHERE id = 3;
+        //SELECT & FROM employee;
+        //\q
+
     }
 }
